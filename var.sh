dd5ebe1cb3565a72a1e8efc024fa78ef153fc422
Kevin Jean-Baptiste <kevin.jb1215@outlook.com>
2026-06-28 15:57:37 -0400

    Create var.sh

---
+++ var.sh
@@ -0,0 +1,12 @@
+{
+  "appId": "<your-app-id>",
+  "displayName": "az-demo",
+  "password": "<your-client-secret>",
+  "tenant": "<your-tenant-id>"
+}
+
+
+export ARM_CLIENT_ID="<your-app-id>"
+export ARM_CLIENT_SECRET="<your-client-secret>"
+export ARM_SUBSCRIPTION_ID="<your-subscription-id>"
+export ARM_TENANT_ID="<your-tenant-id>"
